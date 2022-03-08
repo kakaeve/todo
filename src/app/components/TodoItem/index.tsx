026152d90b7d8c1017e000a007ae65c3c0eafb59
kakaeve <zkzkdlqm@gmail.com>
2022-03-09 07:27:23 +0900

    22.03.09 reducer 기본

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import Block from '../Block';
+import CircleButton from '../Button/CircleButton';
 import Checkbox from '../Checkbox';
+import TodoInput from '../Todoinput';
 
 const Box = styled.div`
   display: flex;
@@ -9,6 +11,17 @@ const Box = styled.div`
   width: 100%;
   font-size: 1.2em;
   border-bottom: 1px solid #eee;
+
+  & > .delete-button {
+    display: none;
+  }
+
+  &:hover {
+    padding: 10px 15px 10px 25px;
+    & > .delete-button {
+      display: flex;
+    }
+  }
 `;
 
 const TodoContent = styled.span<{ checked: boolean }>`
@@ -26,9 +39,31 @@ const TodoContent = styled.span<{ checked: boolean }>`
 export default function TodoItem({ todo }: { todo: ITodoItem }) {
   return (
     <Box>
-      <Checkbox checked={todo.completed} />
-      <Block marginLeft="10px" />
-      <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+      <div style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+        <Checkbox checked={todo.completed} />
+        <Block marginLeft="10px" />
+        {todo.editing ? (
+          <TodoInput />
+        ) : (
+          <TodoContent checked={todo.completed}>{todo.content}</TodoContent>
+        )}
+      </div>
+      <CircleButton
+        className="delete-button"
+        onClick={() => {}}
+        Icon={() => (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
+          </svg>
+        )}
+      />
     </Box>
   );
 }
--- src/app/components/Todoinput/index.tsx
+++ src/app/components/Todoinput/index.tsx
@@ -7,37 +7,55 @@ const Input = styled.input`
   outline: 0;
 `;
 
-const Box = styled.div`
+const Box = styled.div<{ isEditing?: boolean }>`
   display: flex;
   align-items: center;
-  padding: 15px 25px;
-  width: 100%auto;
+  padding: ${props => (props.isEditing ? '5px 0px' : '15px 25px')};
+  width: 100%;
   font-size: 1.1em;
   border-bottom: 1px solid #eee;
 `;
 
 export default function TodoInput({
   setTodoList,
+  isEditing,
+  editContent,
+  editModeTodo,
+  editTodo,
 }: {
-  setTodoList: (todo: ITodoItem) => void;
+  setTodoList?: (todo: ITodoItem) => void;
+  isEditing?: boolean;
+  editContent?: string;
+  editTodo?: (content: string) => void;
+  editModeTodo?: () => void;
 }) {
   const [content, setContent] = React.useState<string>('');
   return (
-    <Box>
+    <Box isEditing={isEditing}>
       <Input
         placeholder="할일을 입력해 주세요"
         value={content}
+        onBlur={e => {
+          if (e.currentTarget === e.target) {
+            editTodo && editTodo(content);
+          }
+        }}
         onChange={e => setContent(e.target.value)}
         onKeyPress={e => {
           if (content === '') return;
           if (e.key !== 'Enter' && e.key !== 'NumpadEnter') return;
-          setTodoList({
-            id: '0',
-            content: content,
-            completed: false,
-            editing: false,
-          });
-          setContent('');
+          if (isEditing) {
+            editTodo && editTodo(content);
+          } else {
+            setTodoList &&
+              setTodoList({
+                id: '0',
+                content: content,
+                completed: false,
+                editing: false,
+              });
+            setContent('');
+          }
         }}
       />
     </Box>
